381dd30e5564b5505ccbe5c99e8713edd01f56e3
Wilson <paschendalemaiden@hotmail.com>
2024-06-26 20:54:26 -0400

    *agregue un book.json *agregue la funcionalidad de leer la informacion del Json y que lo ponga dentro de los campos que cree

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+class Program
+{
+    public static void Main (string[] args){
+        
+
+        var frutas = new string[]{"Sandia", "Fresa","Mango","Mango de azucar","Mango Tomy"};
+
+        var mangoList = frutas.Where(fruit=>fruit.StartsWith("Mango")).ToList();
+
+        mangoList.ForEach(mango =>Console.WriteLine(mango));
+
+        //Console.WriteLine("Hello, World!");
+    }
+    
+}
+
